43d3b7dcac6ca89c97909d3592ce649b39573b70
Gustavo Henrique <gustavoh.santos735@gmail.com>
2024-07-31 00:53:59 -0300

    feat: add colors

---
+++ packages/tokens/src/colors.ts
@@ -0,0 +1,24 @@
+export const colors = {
+    white: "#FFFFFF",
+    black: "#000000",
+
+    gray100: '#FAF6FC',
+    gray200: '#EEE9EC',
+    gray300: '#A9A9B2',
+    gray400: '#7C7C8A',
+    gray500: '#505059',
+    gray600: '#323238',
+    gray700: '#29292E',
+    gray800: '#202024',
+    gray900: '#121214',
+
+    sapuris100: '#c2aad9',
+    sapuris200: '#685a7c',
+    sapuris300: '#6f5c7c',
+    sapuris400: '#654b8c',
+    sapuris500: '#533b7a',
+    sapuris600: '#4c3768',
+    sapuris700: '#392a46',
+    sapuris800: '#2e2734',
+    sapuris900: '#15141b',
+}
