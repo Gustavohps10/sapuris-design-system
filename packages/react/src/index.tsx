f9142fd7a11fbc534f80dceed90d9798d37dd8a2
Gustavo Henrique <gustavoh.santos735@gmail.com>
2024-07-31 09:48:30 -0300

    chore: add stitches config

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,4 +1,10 @@
 import { colors } from '@sapuris-ui/tokens'
+import { styled } from './styles'
+
+const Button = styled('button', {
+  fontFamily: '$default',
+  backgroundColor: '$sapuris400',
+})
 
 export function App() {
   return <button style={{ backgroundColor: colors.sapuris500 }}></button>
---
+++ packages/react/src/styles/index.ts
@@ -0,0 +1,36 @@
+import {
+  radii,
+  colors,
+  fontSizes,
+  fontWeights,
+  fonts,
+  lineHeights,
+  space,
+} from '@sapuris-ui/tokens'
+import { createStitches, defaultThemeMap } from '@stitches/react'
+
+export const {
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+  config,
+} = createStitches({
+  themeMap: {
+    ...defaultThemeMap,
+    height: 'space',
+    width: 'space',
+  },
+  theme: {
+    colors,
+    fontSizes,
+    fontWeights,
+    fonts,
+    lineHeights,
+    space,
+    radii,
+  },
+})
